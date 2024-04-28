f9ba4845e1cf13dab2ddc9461d1f10fbe1ed3573
Tobias Kohler <tobiaskohler@tobis-macbook-pro.home>
2024-04-28 16:45:12 +0200

    slides and refs

--- CuttingPlanes.py
+++ CuttingPlanes.py
@@ -34,19 +34,19 @@ def coeffs_str(coeffs, syms):
         s += coeff_str(coeffs[i], syms[i])
     return s
 
-def ineq_str(A, b, i):
-    cx, cy =-A[i,0], -A[i,1]
+def ineq_str(a, r):
+    cx, cy =-a[0], -a[1]
     s = [t for t in [coeff_str(cx, 'x'), coeff_str(cy, 'y')] if t]
-    return ' + '.join(s) + ' $\leq$ ' + str(int(-b[i])) if b[i]%1==0 else str(-b[i])
+    return ' + '.join(s) + ' $\leq$ ' + str(int(-r)) if r%1==0 else str(-r)
 
 class MILP:
-    def __init__(self, n=2):
+    def __init__(self, n, n1):
         self.c = np.zeros((n))
         self.A = np.zeros((0,n))
         self.b = np.zeros((0))
         self.B = np.zeros((0,n))
         self.d = np.zeros((0))
-        self.n, self.n1, self.m, self.p = n, n, 0, 0
+        self.n, self.n1, self.m, self.p = n, n1, 0, 0
 
     def set_objective(self, c):
         self.c = np.array(c, dtype=np.float64)
@@ -97,61 +97,101 @@ class MILP:
     def __str__(self):
         return "c = {c}\nA =\n{A}\nb =\n{b}".format(c=self.c,A=self.A,b=self.b)
 
-    def save_plot(self, title:str, sol=None, cuts=(np.array([[]]),np.array([])), mark_last_ineq=False, save_file:str=''):
-        c,B,d = self.c,self.B,self.d
-        A,b = (self.A,self.b) if cuts[1].shape[0]==0 else (np.vstack([self.A, cuts[0]]), np.append(self.b, cuts[1]))
+class Plotter:
+    def __init__(self):
+        pass
 
-        # Get Extents of Feasible Region
-        vs = lineqs(self.A, self.b, show=False)
-        xlim, ylim = [-0, 1.1*np.max(vs[:,0])], [-0, 1.1*np.max(vs[:,1])]   
+    def init_figure(self, milp:MILP, cuts=(np.array([[]]),np.array([]))) -> plt.Figure:
+        A,b = (milp.A,milp.b) if cuts[1].shape[0]==0 else (np.vstack([milp.A, cuts[0]]), np.append(milp.b, cuts[1]))
+        vs = lineqs(milp.A, milp.b, show=False)
+        self.xmin, self.xmax = [-0, 1.1*np.max(vs[:,0])]
+        self.ymin, self.ymax = [-0, 1.1*np.max(vs[:,1])] 
+        self.x_vals = np.linspace(self.xmin, self.xmax, 400)
+        self.y_vals = np.linspace(self.ymin, self.ymax, 400)
+        self.X, self.Y = np.meshgrid(self.x_vals, self.y_vals)
 
-        ratio = (xlim[1]-xlim[0])/(ylim[1]-ylim[0])
-        fig = plt.figure(figsize=(10,10/ratio))
-        assert(len(c) == 2)
-        m = len(b)
+        ratio = (self.xmax - self.xmin) / (self.ymax - self.ymin)
+        return plt.figure(figsize=(10,10/ratio))
 
-        # Generate Meshgrid of Points
-        x_vals = np.linspace(xlim[0], xlim[1], 400)
-        y_vals = np.linspace(ylim[0], ylim[1], 400)
-        X, Y = np.meshgrid(x_vals, y_vals)
+    def plot_objective_function(self, c, levels=20, origin='lower', cmap='viridis', alpha_shade=0.1, alpha_isoline=0.3):
+        '''
+        Plots the objective function c[0]x + c[1]y
+        '''
+        Z = c[0]*self.X + c[1]*self.Y
+        plt.imshow(Z, extent=[self.xmin, self.xmax, self.ymin, self.ymax], origin=origin, cmap=cmap, alpha=alpha_isoline)
+        plt.contour(self.X, self.Y, Z, levels=levels, cmap=cmap, alpha=alpha_shade)
 
-        # Plot Objective Function
-        Z = c[0]*X + c[1]*Y
-        plt.imshow(Z, extent=[xlim[0], xlim[1], ylim[0], ylim[1]], origin='lower', cmap='viridis', alpha=0.3)
-        plt.contour(X, Y, Z, levels=20, cmap='viridis', alpha=0.1)
+    def plot_line(self, a, r, color='g', line_width=2, title=''):
+        '''
+        Plots the line a[0]x + a[1]y = r
+        '''
+        if a[1] != 0:
+            plt.plot(self.x_vals, (r - a[0]*self.x_vals) / a[1], color+'-', lw=line_width, label=title)
+        elif a[0] != 0:
+            plt.axvline(x=r / a[0], color=color, linestyle='-', lw=line_width, label=title)
+
+    def plot_feasible_region(self, milp:MILP, cuts=(np.array([[]]),np.array([]))):
+        '''
+        Shades the feasible region A(x,y)' >= b and cuts[0](x,y)' >= cuts[1]
+        '''
+        A,b = (milp.A,milp.b) if cuts[1].shape[0]==0 else (np.vstack([milp.A, cuts[0]]), np.append(milp.b, cuts[1]))
+        feasible_cmap = matplotlib.colors.ListedColormap(['white', 'lime'])
+        feasible_region = np.all(A[:,0,None,None]*self.X + A[:,1,None,None]*self.Y >= b[:,None,None], axis=0)
+        plt.contourf(self.X, self.Y, feasible_region, cmap=feasible_cmap, alpha=0.2)
+
+    def plot_integer_points(self, n1:int, color='blue'):
+        '''
+        Highlights the integer constraints.
+        If n1=2, x and y must be integer
+        If n1=1, only x must be integer
+        '''
+        if n1 == 2:
+            x_int = np.arange(np.ceil(self.xmin), np.floor(self.xmax)+1, 1)
+            y_int = np.arange(np.ceil(self.ymin), np.floor(self.ymax)+1, 1)
+            X_int, Y_int = np.meshgrid(x_int, y_int)
+            plt.scatter(X_int, Y_int, s=40, color=color)
+        elif n1 == 1:
+            for x in range(int(np.ceil(self.xmin)), int(np.floor(self.xmax)+1)):
+                plt.axvline(x=x, color=color, linestyle='-', alpha=1, lw=2)
+
+    def plot_point(self, x, y, color='r'):
+        plt.plot(x, y, color+'o', markersize=10, label='(x,y)=(%g, %g)' % (x, y))
+
+
+    def plot_milp(self, milp:MILP, title:str='MILP', optimal_solution=None, cuts=(np.array([[]]),np.array([])), show_legend=True, save_file:str=''):
+        A,b = (milp.A,milp.b) if cuts[1].shape[0]==0 else (np.vstack([milp.A, cuts[0]]), np.append(milp.b, cuts[1]))
+
+        # Initialize figure
+        fig = self.init_figure(milp, cuts)
+
+        # Plot objective function
+        self.plot_objective_function(milp.c)
 
         # Plot Inequality Constraints
-        for i in range(m):
-            if A[i,1] != 0:
-                plt.plot(x_vals, (b[i] - A[i,0]*x_vals) / A[i,1], 'r-' if (mark_last_ineq and i==m-1) else 'g-', lw=2, label=ineq_str(A,b,i))
-            elif A[i,0] != 0:
-                plt.axvline(x=b[i] / A[i, 0], color='r' if (mark_last_ineq and i==m-1) else 'g', linestyle='-', lw=2, label=ineq_str(A,b,i))
+        for i in range(len(b)):
+            self.plot_line(A[i], b[i], color='g', title=ineq_str(A[i], b[i]))
 
         # Shade Feasible Region
-        feasible_cmap = matplotlib.colors.ListedColormap(['white', 'lime'])
-        feasible_region = np.all(A[:,0,None,None]*X + A[:,1,None,None]*Y >= b[:,None,None], axis=0)
-        plt.contourf(X, Y, feasible_region, cmap=feasible_cmap, alpha=0.2)
+        self.plot_feasible_region(milp, cuts)
 
         # Plot Integer Points
-        x_int = np.arange(np.ceil(xlim[0]), np.floor(xlim[1])+1, 1)
-        y_int = np.arange(np.ceil(ylim[0]), np.floor(ylim[1])+1, 1)
-        X_int, Y_int = np.meshgrid(x_int, y_int)
-        plt.scatter(X_int, Y_int, s=40, color='blue', label='Integers')
+        self.plot_integer_points(milp.n1)
 
         # Plot Optimal Solution
-        if sol is not None:
-            plt.plot(sol[0],sol[1],'ro', markersize=10, label='(x,y)=(%g, %g)' % (sol[0],sol[1]))
+        if optimal_solution is not None:
+            self.plot_point(optimal_solution[0], optimal_solution[1])
 
         # Labels and Stuff
-        plt.legend(bbox_to_anchor=(1.05,1), loc="upper left")
-        plt.xlabel('x')
-        plt.ylabel('y')
+        if show_legend:
+            plt.legend(bbox_to_anchor=(1.05,1), loc="upper left")
+            plt.xlabel('x')
+            plt.ylabel('y')
         plt.title(title)
-        plt.grid(color='gray', linestyle='-', linewidth=1)
-        plt.xticks(np.arange(np.floor(xlim[0]), xlim[1], 1))
-        plt.yticks(np.arange(np.floor(ylim[0]), ylim[1], 1))
-        plt.xlim(xlim)
-        plt.ylim(ylim)
+        plt.grid(color='gray', linestyle='--', linewidth=1)
+        plt.xticks(np.arange(np.floor(self.xmin), self.xmax, 1))
+        plt.yticks(np.arange(np.floor(self.ymin), self.ymax, 1))
+        plt.xlim((self.xmin, self.xmax))
+        plt.ylim((self.ymin, self.ymax))
 
         if save_file:
             plt.savefig(save_file, bbox_inches='tight', transparent=True) # Save Plot
